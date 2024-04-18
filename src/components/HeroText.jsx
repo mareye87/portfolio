68793aa888ace9977ffe25e97451b4c4a822eafb
mareye87 <marekgszmyd@gmail.com>
2024-04-18 15:38:59 +0100

    bg path fixed

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef } from "react";
 import { SlArrowDown } from "react-icons/sl";
-import bgImgMmobile from "../assets/images/landing-bg-mobile2.jpg";
-import bgImgDesktop from "../assets/images/landing-bg.jpg";
 
 const HeroText = () => {
   const bgRef = useRef();
@@ -25,7 +23,7 @@ const HeroText = () => {
   return (
     <div
       ref={bgRef}
-      className="pt-24 pb-10 px-2 w-full h-screen bg-[url('landing-bg-mobile2.jpg')] sm:bg-[url('landing-bg.jpg')] bg-cover bg-top bg-fixed   "
+      className="pt-24 pb-10 px-2 w-full h-screen bg-[url('/landing-bg-mobile2.jpg')] sm:bg-[url('/landing-bg.jpg')] bg-cover bg-top bg-fixed   "
     >
       <div className="h-full flex flex-col justify-between ">
         <div className="flex flex-col justify-center text-center gap-y-4 text-slate-900">
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,7 +3,7 @@ import { ContactUs } from "../components/ContactUs";
 
 function ContactPage() {
   return (
-    <div className="h-[calc(100vh-40px)]  py-20 px-2 lg:px-24 bg-[url('wall-bg.jpg')] bg-cover bg-center">
+    <div className="h-[calc(100vh-40px)]  py-20 px-2 lg:px-24 bg-[url('/wall-bg.jpg')] bg-cover bg-center">
       <div className="mt-12 w-full sm:w-1/2 mx-auto ">
         <ContactUs />
       </div>
